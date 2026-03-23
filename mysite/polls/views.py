d38aa2c72aba7399fbfb4d9228998e5a650ac68e
Jacob <jacobbibat24@gmail.com>
2026-03-23 15:37:04 +0000

    created html and views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+# import all your models
+from .models import (
+    Salesperson,
+    Mechanic,
+    Customer,
+    Car,
+    SalesInvoice,
+    Service,
+    ServiceTicket,
+    ServiceMechanic,
+    Parts,
+    PartsUsed
+)
+
+
+def home(request):
+    return render(request, "polls/home.html", {
+        "salespeople": Salesperson.objects.all(),
+        "mechanics": Mechanic.objects.all(),
+        "customers": Customer.objects.all(),
+        "cars": Car.objects.all(),
+        "invoices": SalesInvoice.objects.all(),
+        "services": Service.objects.all(),
+        "tickets": ServiceTicket.objects.all(),
+        "servicemechanics": ServiceMechanic.objects.all(),
+        "parts": Parts.objects.all(),
+        "partsused": PartsUsed.objects.all(),
+    })
